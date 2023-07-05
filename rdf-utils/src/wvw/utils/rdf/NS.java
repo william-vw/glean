977125b987801bd4ce46843b65115d2dbb300ddd
William Van Woensel <wvw@Williams-MBP.lan>
2023-07-05 17:37:07 -0400

    ex ns

--- rdf-utils/src/wvw/utils/rdf/NS.java
+++ rdf-utils/src/wvw/utils/rdf/NS.java
@@ -28,6 +28,7 @@ public class NS {
 
 	public static Map<String, String> map;
 
+	public static final String ex = "http://example.org/";
 	public static final String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	public static final String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
 	public static final String owl = "http://www.w3.org/2002/07/owl#";
